4464d8708fd6ed8d1aba7e4de4f2fba43372ab11
GhKama <90391917+Mordecaikama@users.noreply.github.com>
2024-07-01 09:37:20 +0100

    Update home.js
    old fetch removed.

--- src/pages/home.js
+++ src/pages/home.js
@@ -72,28 +72,6 @@ function Home() {
     }
   }
 
-  // const getUser = () => {
-  //   fetch(`${API}/auth/login/success`, {
-  //     method: 'GET',
-  //     credentials: 'include',
-  //     headers: {
-  //       Accept: 'application/json',
-  //       'Content-Type': 'application/json',
-  //       'Access-Control-Allow-Credentials': true,
-  //     },
-  //   })
-  //     .then((response) => {
-  //       if (response.status === 200) return response.json()
-  //       throw new Error('authentication has been failed!')
-  //     })
-  //     .then((resObject) => {
-  //       // console.log(resObject.user)
-  //       setUser(resObject.user)
-  //     })
-  //     .catch((err) => {
-  //       console.log(err, 'big error here')
-  //     })
-  // }
 
   const getUser = async () => {
     const res = await axios.get(`${API}/auth/login/success`)
